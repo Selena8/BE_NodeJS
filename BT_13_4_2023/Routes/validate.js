aaa79d6e9f59e91c5b3a32fb6cf9abb0ffdb5ee9
Selena8 <nguyet982002@gmail.com>
2023-04-18 11:58:40 +0700

    Test

--- BT_13_4_2023/Routes/validate.js
+++ BT_13_4_2023/Routes/validate.js
@@ -2,10 +2,10 @@ function validate(req, res, next) {
     const name = req.body.name
     const age = req.body.age
     if(age < 0) {
-        return res.status(404).json({"message": "Tuoi ban nhap bi loi!"})
+        return res.status(404).json({"message": "Hay nhap lai tuoi!"})
     }
     else if(!/^[a-zA-Z ]+$/.test(name)) {
-        return res.status(404).json({"message": "Ten ban nhap bi loi!"})
+        return res.status(404).json({"message": "Hay nhap lai ten!"})
     }
     next()
 }
